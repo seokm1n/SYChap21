ff6da435e745f1ccb5c86fa7804d7d84fee7d834
suckm <suckm@DESKTOP-DUMRNUO>
2025-02-28 11:00:25 +0900

    .

--- board/src/sql/query.sql
+++ board/src/sql/query.sql
@@ -1,6 +1,6 @@
-create table board.member (
-	memberid varchar(50) primary key,
-	name varchar(50) not null,
-	password varchar(10) not null,
-	regdate datetime not null
-) engine=InnoDB default character set = utf8;
+CREATE TABLE member (
+    memberid VARCHAR2(50) PRIMARY KEY,
+    name VARCHAR2(50) NOT NULL,
+    password VARCHAR2(10) NOT NULL,
+    regdate DATE DEFAULT SYSDATE NOT NULL
+);
